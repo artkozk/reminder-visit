758d675f2f15dc1ba3bc33eb90979d4446878ef5
artkozk <art.koz.r@yandex.ru>
2024-08-15 14:29:36 +0300

    у меня нефига не получилось и я делаю все в одном файле

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from config import token
 import telebot
 from telebot import types
-from functions import *
 
 bot = telebot.TeleBot(token)
 
@@ -15,5 +14,9 @@ def Start(message):
 
     bot.send_message(chat_id=message.chat.id, text='привет, это бот молодёжных служений в городе Липецке', reply_markup=markup)
     bot.register_next_step_handler(message, Firs_keyboard_check, message)
-
+def Firs_keyboard_check(message):
+    if message.text == 'регистрация':
+        bot.send_mesasge(message.chat_id, text='Напишите ваше имя')
+    else:
+        bot.send_message(message.chat_id, text='')
 bot.polling()
