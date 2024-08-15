ba37b41eafc4ffc6d06e5d0364a8e11e525d087e
artkozk <art.koz.r@yandex.ru>
2024-08-15 13:46:44 +0300

    первая фиксация

---
+++ main.py
@@ -0,0 +1,17 @@
+from config import token
+import telebot
+from telebot import types
+from functions import Firs_keyboard_check
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    sing_in_button = types.KeyboardButton(text='регистрация')
+    markup.add(sing_in_button)
+    bot.send_message(chat_id=message.chat.id, text='привет, это бот молодёжных служений в городе Липецке', reply_markup=markup)
+    bot.register_next_step_handler(message, Firs_keyboard_check)
+
+bot.polling()
