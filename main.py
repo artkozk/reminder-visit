cff723fc3d6be57ad68398379b09e5b15a22fce7
artkozk <art.koz.r@yandex.ru>
2024-08-21 23:02:27 +0300

    изменнил ссылку доната

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def Funk(message):
 
 def Funk_check(message):
     if message.text == 'пожертвовать':
-        bot.send_message(message.chat.id, 'Пожертвование можно совершить по этой ссылке:\nhttps://www.tinkoff.ru/cf/6CxmYK1srAU\nСпасибо за ваше открытое сердце!')
+        bot.send_message(message.chat.id, 'Пожертвование можно совершить по этой ссылке: https://www.tinkoff.ru/rm/beshkarev.daniil1/Zv7pR81683\n\nСпасибо за ваше открытое сердце!')
         bot.register_next_step_handler(message, Funk_check)
     elif message.text == 'что читать сегодня?':
         Read(message.chat.id)
